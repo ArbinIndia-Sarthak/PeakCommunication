6b49c00053de9a4b945afd2bc74b53b6c642ed0c
ArbinIndia-Sudarshan <161278920+ArbinIndia-Sudarshan@users.noreply.github.com>
2025-02-11 15:17:10 +0530

    ReadCAN normal messages

--- PeakCommunication/ReceiveCANMessages.cs
+++ PeakCommunication/ReceiveCANMessages.cs
@@ -1,68 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Peak.Can.Basic.BackwardCompatibility;
-using static PeakCommunication.PcanBasic;
+﻿using static PeakCommunication.PcanBasic;
 
 namespace PeakCommunication
 {
     public class ReceiveCAN
     {
-        public class ReceiveCanMessage
+        public static void InitialiseCANReceiver()
         {
-            public static void InitialiseCANReceiver()
+            var channel = TPCANHandle.PCAN_USB;  // Change to the proper channel
+            string bitrate = PcanBasic.PCAN_BAUD_500K;
+
+            uint status1 = PcanBasic.CAN_Initialize(channel, bitrate, 0, 0, 0);
+
+            if (status1 != (uint)PcanBasic.TPCANStatus.PCAN_ERROR_OK)
             {
-                var channel = TPCANHandle.PCAN_USB;  // Change to the proper channel
-                string bitrateFD = "f_clock_mhz=20, nom_brp=4, nom_tseg1=6, nom_tseg2=3, nom_sjw=1, data_brp=2, data_tseg1=3, data_tseg2=1, data_sjw=1";
+                Console.WriteLine("Error initializing PCAN: " + status1);
+                return;
+            }
+            Console.WriteLine("PCAN receiver initialized successfully!");
 
-                PcanBasic.TPCANStatus status1 = PcanBasic.InitializeFD(channel, bitrateFD);
-
-                if (status1 != PcanBasic.TPCANStatus.PCAN_ERROR_OK)
-                {
-                    Console.WriteLine("Error initializing PCAN: " + status1);
-                    return;
-                }
-                Console.WriteLine("PCAN receiver initialized successfully!");
-
-                // Start receiving CAN messages
-                while (true)
-                {
-                    ReceiveCANMessage(channel);
-                    Thread.Sleep(20);
-                }
-
-                // Uninitialize PCAN after use
-                CAN_Uninitialize(channel);
+            // Start receiving CAN messages
+            while (true)
+            {
+                ReceiveCANMessage(channel);
+                Thread.Sleep(20);
             }
 
-            static void ReceiveCANMessage(TPCANHandle Channel)
+            // Uninitialize PCAN after use
+            CAN_Uninitialize(channel);
+        }
+
+        static void ReceiveCANMessage(TPCANHandle Channel)
+        {
+            PcanBasic.TPCANMsg message;
+            nint timestamp = 0;
+            message.DLC = 8;
+            uint status = PcanBasic.CAN_Read(Channel, out message, timestamp);
+
+            if (status == (uint)PcanBasic.TPCANStatus.PCAN_ERROR_OK)
             {
-                PcanBasic.TPCANMsgFD message;
-                nint timestamp;
-                message.DLC = 64;
-                PcanBasic.TPCANStatus status = PcanBasic.CAN_ReadFD(Channel, out message, out timestamp);
+                Console.WriteLine("Message received!");
+                Console.WriteLine("ID: 0x" + message.ID.ToString("X"));
+                Console.WriteLine("DLC: " + message.DLC);
+                Console.Write("Data: ");
 
-                if (status == PcanBasic.TPCANStatus.PCAN_ERROR_OK)
+                for (int i = 0; i < message.DLC; i++)
                 {
-                    Console.WriteLine("Message received!");
-                    Console.WriteLine("ID: 0x" + message.ID.ToString("X"));
-                    Console.WriteLine("DLC: " + message.DLC);
-                    Console.Write("Data: ");
-
-                    for (int i = 0; i < message.DLC; i++)
-                    {
-                        Console.Write(message.DATA[i].ToString("X2") + " ");
-                    }
-                    Console.WriteLine();
+                    Console.Write(message.DATA[i].ToString("X2") + " ");
                 }
-                else
-                {
-                    Console.WriteLine("Receive Error: 0x" + status.ToString("X"));
-                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Receive Error: 0x" + status.ToString("X"));
             }
         }
     }
-
 }
+
